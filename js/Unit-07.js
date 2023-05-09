5b0319c7e4d52b3047d10dbf0d7298f00d4eeecb
Gavin81 <g_eyquem@hotmail.com>
2023-05-09 20:18:18 +0100

    Update Unit-07.js

--- js/Unit-07.js
+++ js/Unit-07.js
@@ -72,3 +72,47 @@ const dailyOptions = {
         }
     }
 };
+
+
+let dailyChart = new Chart(dailyCanvas, {
+    type:'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+const mobileCanvas = document.getElementById("daily-chart");
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
